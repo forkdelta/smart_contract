7356ee9c4b53b9bd6403870364a3aa1fc576ee14
BigBrother <dunford.jonathon@gmail.com>
2018-03-04 17:56:54 -0700

    Remove account levels, remove fee rebates, change ed to fd

--- smart_contract.sol
+++ smart_contract.sol
@@ -128,33 +128,11 @@ contract ReserveToken is StandardToken, SafeMath {
   }
 }
 
-contract AccountLevels {
-  //given a user, returns an account level
-  //0 = regular user (pays take fee and make fee)
-  //1 = market maker silver (pays take fee, no make fee, gets rebate)
-  //2 = market maker gold (pays take fee, no make fee, gets entire counterparty's take fee as rebate)
-  function accountLevel(address user) constant returns(uint) {}
-}
-
-contract AccountLevelsTest is AccountLevels {
-  mapping (address => uint) public accountLevels;
-
-  function setAccountLevel(address user, uint level) {
-    accountLevels[user] = level;
-  }
-
-  function accountLevel(address user) constant returns(uint) {
-    return accountLevels[user];
-  }
-}
-
-contract EtherDelta is SafeMath {
+contract ForkDelta is SafeMath {
   address public admin; //the admin address
   address public feeAccount; //the account that will receive fees
-  address public accountLevelsAddr; //the address of the AccountLevels contract
   uint public feeMake; //percentage times (1 ether)
   uint public feeTake; //percentage times (1 ether)
-  uint public feeRebate; //percentage times (1 ether)
   mapping (address => mapping (address => uint)) public tokens; //mapping of token addresses to mapping of account balances (token=0 means Ether)
   mapping (address => mapping (bytes32 => bool)) public orders; //mapping of user accounts to mapping of order hashes to booleans (true = submitted by user, equivalent to offchain signature)
   mapping (address => mapping (bytes32 => uint)) public orderFills; //mapping of user accounts to mapping of order hashes to uints (amount of order that has been filled)
@@ -165,13 +143,11 @@ contract EtherDelta is SafeMath {
   event Deposit(address token, address user, uint amount, uint balance);
   event Withdraw(address token, address user, uint amount, uint balance);
 
-  function EtherDelta(address admin_, address feeAccount_, address accountLevelsAddr_, uint feeMake_, uint feeTake_, uint feeRebate_) {
+  function ForkDelta(address admin_, address feeAccount_, uint feeMake_, uint feeTake_) {
     admin = admin_;
     feeAccount = feeAccount_;
-    accountLevelsAddr = accountLevelsAddr_;
     feeMake = feeMake_;
     feeTake = feeTake_;
-    feeRebate = feeRebate_;
   }
 
   function() {
@@ -181,11 +157,6 @@ contract EtherDelta is SafeMath {
   function changeAdmin(address admin_) {
     if (msg.sender != admin) throw;
     admin = admin_;
-  }
-
-  function changeAccountLevelsAddr(address accountLevelsAddr_) {
-    if (msg.sender != admin) throw;
-    accountLevelsAddr = accountLevelsAddr_;
   }
 
   function changeFeeAccount(address feeAccount_) {
@@ -201,14 +172,8 @@ contract EtherDelta is SafeMath {
 
   function changeFeeTake(uint feeTake_) {
     if (msg.sender != admin) throw;
-    if (feeTake_ > feeTake || feeTake_ < feeRebate) throw;
+    if (feeTake_ > feeTake) throw;
     feeTake = feeTake_;
-  }
-
-  function changeFeeRebate(uint feeRebate_) {
-    if (msg.sender != admin) throw;
-    if (feeRebate_ < feeRebate || feeRebate_ > feeTake) throw;
-    feeRebate = feeRebate_;
   }
 
   function deposit() payable {
@@ -265,15 +230,9 @@ contract EtherDelta is SafeMath {
   function tradeBalances(address tokenGet, uint amountGet, address tokenGive, uint amountGive, address user, uint amount) private {
     uint feeMakeXfer = safeMul(amount, feeMake) / (1 ether);
     uint feeTakeXfer = safeMul(amount, feeTake) / (1 ether);
-    uint feeRebateXfer = 0;
-    if (accountLevelsAddr != 0x0) {
-      uint accountLevel = AccountLevels(accountLevelsAddr).accountLevel(user);
-      if (accountLevel==1) feeRebateXfer = safeMul(amount, feeRebate) / (1 ether);
-      if (accountLevel==2) feeRebateXfer = feeTakeXfer;
-    }
     tokens[tokenGet][msg.sender] = safeSub(tokens[tokenGet][msg.sender], safeAdd(amount, feeTakeXfer));
-    tokens[tokenGet][user] = safeAdd(tokens[tokenGet][user], safeSub(safeAdd(amount, feeRebateXfer), feeMakeXfer));
-    tokens[tokenGet][feeAccount] = safeAdd(tokens[tokenGet][feeAccount], safeSub(safeAdd(feeMakeXfer, feeTakeXfer), feeRebateXfer));
+    tokens[tokenGet][user] = safeAdd(tokens[tokenGet][user], safeSub(amount, feeMakeXfer));
+    tokens[tokenGet][feeAccount] = safeAdd(tokens[tokenGet][feeAccount], safeAdd(feeMakeXfer, feeTakeXfer));
     tokens[tokenGive][user] = safeSub(tokens[tokenGive][user], safeMul(amountGive, amount) / amountGet);
     tokens[tokenGive][msg.sender] = safeAdd(tokens[tokenGive][msg.sender], safeMul(amountGive, amount) / amountGet);
   }
